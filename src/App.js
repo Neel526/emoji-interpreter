d972d2e52e131d9cbdab9abbaec5fa4a0dba4a42
Neel526 <63090511+Neel526@users.noreply.github.com>
2021-07-19 00:03:36 +0530

    Map Added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ const emojiDictionary = {
   "😑": "annoyance"
 };
 
+const emojis = Object.keys(emojiDictionary);
+
 export default function App() {
   var [emoji, setEmoji] = useState("");
   var [meaning, setMeaning] = useState("Emoji's");
@@ -25,12 +27,32 @@ export default function App() {
     }
   }
 
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
   return (
     <div className="App">
       <h1>inside outttt</h1>
       <input onChange={changeHandler} />
       <div> {emoji} </div>
       <div> {meaning} </div>
+      {
+        /** Bonus feature; if time permmits */
+        /**
+         * concepts to cover: mapping a list
+         * click handler on list item
+         */
+        emojis.map((emoji) => (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        ))
+      }
     </div>
   );
 }
